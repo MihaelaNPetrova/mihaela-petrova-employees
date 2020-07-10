ef3bfc039d2096b3900ac72f3b37380416816659
Mihaela <Mihaela@DESKTOP-QH1JRJR>
2020-07-10 10:25:40 +0300

    Add project files.

---
+++ Employees/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Employees
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.EmpID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProjectID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateFrom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateTo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmpID,
+            this.ProjectID,
+            this.DateFrom,
+            this.DateTo});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(622, 178);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // EmpID
+            // 
+            this.EmpID.HeaderText = "EmpID";
+            this.EmpID.Name = "EmpID";
+            // 
+            // ProjectID
+            // 
+            this.ProjectID.HeaderText = "ProjectID";
+            this.ProjectID.Name = "ProjectID";
+            // 
+            // DateFrom
+            // 
+            this.DateFrom.HeaderText = "DateFrom";
+            this.DateFrom.Name = "DateFrom";
+            // 
+            // DateTo
+            // 
+            this.DateTo.HeaderText = "DateTo";
+            this.DateTo.Name = "DateTo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove Null";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(452, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Find";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(744, 306);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView2.Location = new System.Drawing.Point(52, 351);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(622, 178);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "EmpID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "ProjectID";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "DateFrom";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "DateTo";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Show All Employees";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "If there are NULL values";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 255);
+            this.label3.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 34);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Find two employees working on one project the most ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(716, 255);
+            this.label4.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Period of working together";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(951, 580);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(969, 627);
+            this.MinimumSize = new System.Drawing.Size(969, 627);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmpID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProjectID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateFrom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateTo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Employees/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+
+namespace Employees
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void toDateFormat()
+        {
+            dataGridView1.Columns[2].DefaultCellStyle.Format = "yyyy-MM-dd";
+            dataGridView1.Columns[3].DefaultCellStyle.Format = "yyyy-MM-dd";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("Resources/Employees.txt");
+            string[] values;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split(',');
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+
+                toDateFormat();
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime d1;
+            DateTime d2;
+
+
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[3].Value.Equals("NULL"))
+
+                {
+                    d1 = DateTime.Today;
+                    this.dataGridView1.Rows[i].Cells[3].Value = DateTime.Today;
+                }
+                else
+
+                    d1 = Convert.ToDateTime(dataGridView1.Rows[i].Cells[3].Value);
+
+                d2 = Convert.ToDateTime(dataGridView1.Rows[i].Cells[2].Value);
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+
+        { 
+
+            DateTime endDate = DateTime.MinValue;
+            DateTime begDate = DateTime.MinValue;
+            int rowCount = dataGridView1.RowCount;
+            int colCount = dataGridView1.ColumnCount;
+            List<Employees> Empl = new List<Employees>();
+            for (int i = 0; i < rowCount-1; i++)
+            { if (dataGridView1.Rows[i].Cells[1].Value == dataGridView1.Rows[i + 1].Cells[1].Value)
+                {
+                   for(int j = 0; j < colCount; j++)
+                    {
+                  Empl.Add(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                    }
+                }
+                            toDateFormat();
+
+            
+
+                   
+                    DateTime endDateOne = Convert.ToDateTime(dataGridView2.Rows[i].Cells[3].Value);
+                    DateTime endDateTwo = Convert.ToDateTime(dataGridView2.Rows[1 + i].Cells[3].Value);
+                    DateTime begDateOne = Convert.ToDateTime(dataGridView2.Rows[i].Cells[2].Value);
+                    DateTime begDateTwo = Convert.ToDateTime(dataGridView2.Rows[1 + i].Cells[2].Value);
+                    if (dataGridView2.Rows[i].Cells[1].Value == dataGridView1.Rows[i + 1].Cells[1].Value)
+                    {
+
+                        if (endDateOne > endDateTwo)
+                        {
+                            endDate = endDateTwo;
+                        }
+                        else
+                        { endDate = endDateOne; }
+                        if (begDateOne > begDateTwo)
+                        {
+                            begDate = begDateOne;
+                        }
+                        else
+                        { begDate = begDateTwo; }
+                    }
+                TimeSpan period = endDate - begDate;
+                textBox1.Text = endDate.ToString();
+                   
+           
+                            
+                        }
+                    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+
+            }
+
+       
+   
